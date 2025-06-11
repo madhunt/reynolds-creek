999a86b4857215414053dda9109f8c9b59a0a475
madhunt <madannahunt@gmail.com>
2025-06-11 11:05:01 -0600

    reproduced jeff's figures successfully

--- code/plot_code/plot_network_response.py
+++ code/plot_code/plot_network_response.py
@@ -21,17 +21,20 @@ def main(path_station_gps):
     # use dem to get zg (see below)
 
     # create meshgrid of all locations
-    #TODO jeff uses stepsize 25 at line 47
     northing = np.arange(4771000, 4777000, step=25)
-    easting = np.arange(515000, 520000, step=25)
+    #easting = np.arange(515000, 520000, step=25)
+    northing = np.arange(-5000, 5000, step=25)
+    easting = np.arange(-2500, 7500, step=25)
     [xg, yg] = np.meshgrid(easting, northing)
     #zg = dem # line 61
 
     fig_net, ax_net = plt.subplots(1, 1)
     ax_net.set_aspect('equal')
     ax_net.grid(visible=True)
-    ax_net.set_xlim(515000, 520000)
-    ax_net.set_ylim(4771000, 4777000)
+    #ax_net.set_xlim(515000, 520000)
+    #ax_net.set_ylim(4771000, 4777000)
+    ax_net.set_xlim(-2500, 7500)
+    ax_net.set_ylim(-5000, 5000)
     ax_net.set_xlabel("Easting (m)")
     ax_net.set_ylabel("Northing (m)")
 
@@ -39,17 +42,28 @@ def main(path_station_gps):
 
     #TODO add contour lines from DEM to fig_net (line 66)
 
-    arr_list = ["TOP", "JDNA", "JDNB", "JDSA", "JDSB"]
-    arr_cols = ['red', 'orange', 'yellow', 'green', 'blue']
-    # read in station coords
-    path_coords = glob.glob(os.path.join(path_station_gps, "*.csv"))[0]
-    coords = pd.read_csv(path_coords)
-    # convert coordinates to easting/northing
-    coords[["Easting", "Northing", "Zone Num", "Zone Lett"]] = coords.apply(lambda x: utm.from_latlon(x["Latitude"], x["Longitude"]), axis=1).to_list()
-    # only save coords for arrays we are using
-    pattern = '|'.join(arr_list)
-    coords = coords[coords["Station"].str.contains(pattern)]
-    coords = coords.reset_index()
+    #arr_list = ["TOP", "JDNA", "JDNB", "JDSA", "JDSB"]
+    #arr_cols = ['red', 'orange', 'yellow', 'green', 'blue']
+    ## read in station coords
+    #path_coords = glob.glob(os.path.join(path_station_gps, "*.csv"))[0]
+    #coords = pd.read_csv(path_coords)
+    ## convert coordinates to easting/northing
+    #coords[["Easting", "Northing", "Zone Num", "Zone Lett"]] = coords.apply(lambda x: utm.from_latlon(x["Latitude"], x["Longitude"]), axis=1).to_list()
+    ## only save coords for arrays we are using
+    #pattern = '|'.join(arr_list)
+    #coords = coords[coords["Station"].str.contains(pattern)]
+    #coords = coords.reset_index()
+
+    coords = pd.DataFrame(columns=['Station', 'Easting', 'Northing'])
+    coords['Station'] = ['KINA1', 'KINA2',
+                         'KINB3', 'KINB4', 'KINB6',
+                         'KINC7', 'KINC8', 'KINC9']
+    coords['Easting'] = [2365, 2400, 2580, 2600, 2590, 2105, 2120, 2140]
+    coords['Northing'] = [1815, 1785, 355, 360, 385, -1040, -1060, -1015]
+
+    arr_list = ['KINA', 'KINB', 'KINC']
+    arr_cols = ['yellow', 'red', 'blue']
+
     
     for i, arr_str in enumerate(arr_list):
         coords_sub = coords[coords["Station"].str.contains(arr_str)]
@@ -83,19 +97,34 @@ def main(path_station_gps):
         #test_coords = test_coords - center
         #kxs, kys, K = array_response(test_coords, n_wavenum=101, wavelen_max=15)        
         #TODO add z coord to these
-        test_coords = coords_sub[["Longitude", "Latitude"]].to_numpy()
-        test_coords = np.insert(test_coords, 2, 0, axis=1)
+        #test_coords = coords_sub[["Longitude", "Latitude"]].to_numpy()
+        #test_coords = np.insert(test_coords, 2, 0, axis=1)
 
 
 
         # choose wavenum at 24Hz in rad/km as per Jake eq code
         klim = 2 * np.pi * 24 / 0.330   
         kstep = 1
+        #array_response = obspy.signal.array_analysis.array_transff_wavenumber(test_coords,
+        #                                                                      klim=klim,
+        #                                                                      kstep=kstep,
+        #                                                                      coordsys='lonlat')
+        test_coords = coords_sub[["Easting", "Northing"]].to_numpy()
+        test_coords = test_coords / 1000 #obspy wants km
+        test_coords = np.insert(test_coords, 2, 0, axis=1)
+        test_coords = test_coords.astype(np.float64)
         array_response = obspy.signal.array_analysis.array_transff_wavenumber(test_coords,
                                                                               klim=klim,
                                                                               kstep=kstep,
-                                                                              coordsys='lonlat')
-        lim = klim #/100
+                                                                              coordsys='xy')
+        #lim = klim #/100
+        #zlim = [0,1]
+        #res = ax_arr[1,i].imshow(array_response.transpose(), 
+        #                   extent=[-lim, lim, -lim, lim],
+        #                   origin='lower',
+        #                   vmin=zlim[0], vmax=zlim[1],
+        #                   cmap='plasma')
+        lim = 0.4
         zlim = [0,1]
         res = ax_arr[1,i].imshow(array_response.transpose(), 
                            extent=[-lim, lim, -lim, lim],
@@ -107,7 +136,8 @@ def main(path_station_gps):
 
         circle_freq = [4, 8, 24]
         for f in circle_freq:
-            r = 2*np.pi*f/0.333
+            #r = 2*np.pi*f/0.333
+            r = 2*np.pi*f/0.333/1000
             theta = np.linspace(0, 2*np.pi, 100)
             ax_arr[1,i].plot(r*np.sin(theta), r*np.cos(theta), 'k--')
 
@@ -130,8 +160,10 @@ def main(path_station_gps):
         #NOTE coords is in order of JDNA, JDNB, JDSA, JDSB, TOP
 
     #subsample meshgrid
-    xgdec = xg[25::25, 25::25]
-    ygdec = yg[25::25, 25::25]
+    #xgdec = xg[25::25, 25::25]
+    #ygdec = yg[25::25, 25::25]
+    xgdec = xg[100:350:50, 67:375:50]
+    ygdec = yg[100:350:50, 67:375:50]
     ax_net.plot(xgdec, ygdec, "ko", markerfacecolor=[1,0.5,0.5])
     for k in range(np.size(xgdec)):
         print(f"k={k} of {np.size(xgdec)}")
@@ -146,43 +178,69 @@ def main(path_station_gps):
         # length of dtmp is 60 bc there are 60 sensors (same len as coords)
         dtmp = np.squeeze(dists[idxy, idxx, :])
 
-        # JDNA idx 0, 1, 2, 3
-        dtmp_jdna = np.diff(dtmp[:4])
-        dtmp_jdna = np.insert(dtmp_jdna, obj=0, values=(dtmp[3]-dtmp[0])) # handle first row (last minus first)
-        restmp_jdna = np.diff(dists[:,:,:4], axis=2)
-        restmp_jdna = np.insert(restmp_jdna, obj=0, axis=2, values=(dists[:,:,3]-dists[:,:,0]))
-        # JDNB idx 4, 5, 6, 7
-        dtmp_jdnb = np.diff(dtmp[4:8])
-        dtmp_jdnb = np.insert(dtmp_jdnb, obj=0, values=(dtmp[7]-dtmp[4]))
-        restmp_jdnb = np.diff(dists[:,:,4:8], axis=2)
-        restmp_jdnb = np.insert(restmp_jdnb, obj=0, axis=2, values=(dists[:,:,7]-dists[:,:,4]))
-        # jdsa idx 8, 9, 10, 11
-        dtmp_jdsa = np.diff(dtmp[8:12])
-        dtmp_jdsa = np.insert(dtmp_jdsa, obj=0, values=(dtmp[11]-dtmp[8]))
-        restmp_jdsa = np.diff(dists[:,:,8:12], axis=2)
-        restmp_jdsa = np.insert(restmp_jdsa, obj=0, axis=2, values=(dists[:,:,11]-dists[:,:,8]))
-        # jdsb idx 12, 13, 14, 15
-        dtmp_jdsb = np.diff(dtmp[12:16])
-        dtmp_jdsb = np.insert(dtmp_jdsb, obj=0, values=(dtmp[15]-dtmp[12]))
-        restmp_jdsb = np.diff(dists[:,:,12:16], axis=2)
-        restmp_jdsb = np.insert(restmp_jdsb, obj=0, axis=2, values=(dists[:,:,15]-dists[:,:,12]))
-        # top idx 16, 17, ... 59
-        dtmp_top = np.diff(dtmp[16:])
-        dtmp_top = np.insert(dtmp_top, obj=0, values=(dtmp[-1]-dtmp[16]))
-        restmp_top = np.diff(dists[:,:,16:], axis=2)
-        restmp_top = np.insert(restmp_top, obj=0, axis=2, values=(dists[:,:,-1]-dists[:,:,16]))
+        dtmp21 = dtmp[1] - dtmp[0]
+        restmp21 = np.abs(dtmp21 - (dists[:,:,1]-dists[:,:,0]))
+        dtmp34 = dtmp[2] - dtmp[3]
+        restmp34 = np.abs(dtmp34 - (dists[:,:,2]-dists[:,:,3]))
+        dtmp46 = dtmp[3] - dtmp[4]
+        restmp46 = np.abs(dtmp46 - (dists[:,:,3]-dists[:,:,4]))
+        dtmp63 = dtmp[4] - dtmp[2]
+        restmp63 = np.abs(dtmp63 - (dists[:,:,4]-dists[:,:,2]))
+        dtmp78 = dtmp[5] - dtmp[6]
+        restmp78 = np.abs(dtmp78 - (dists[:,:,5]-dists[:,:,6]))
+        dtmp89 = dtmp[6] - dtmp[7]
+        restmp89 = np.abs(dtmp89 - (dists[:,:,6]-dists[:,:,7]))
+        dtmp97 = dtmp[7] - dtmp[5]
+        restmp97 = np.abs(dtmp97 - (dists[:,:,7]-dists[:,:,5]))
 
-        # combine all residuals into one matrix
-        drestmp = np.concatenate((restmp_jdna, restmp_jdnb, restmp_jdsa,
-                                           restmp_jdsb, restmp_top), axis=2)
-        # distance residual (square and sum)
-        drestmp = np.sum(drestmp**2, axis=2)
+
+        ## JDNA idx 0, 1, 2, 3
+        #dtmp_jdna = np.diff(dtmp[:4])
+        #dtmp_jdna = np.insert(dtmp_jdna, obj=0, values=(dtmp[3]-dtmp[0])) # handle first row (last minus first)
+        #restmp_jdna = np.diff(dists[:,:,:4], axis=2)
+        #restmp_jdna = np.insert(restmp_jdna, obj=0, axis=2, values=(dists[:,:,3]-dists[:,:,0]))
+        ## JDNB idx 4, 5, 6, 7
+        #dtmp_jdnb = np.diff(dtmp[4:8])
+        #dtmp_jdnb = np.insert(dtmp_jdnb, obj=0, values=(dtmp[7]-dtmp[4]))
+        #restmp_jdnb = np.diff(dists[:,:,4:8], axis=2)
+        #restmp_jdnb = np.insert(restmp_jdnb, obj=0, axis=2, values=(dists[:,:,7]-dists[:,:,4]))
+        ## jdsa idx 8, 9, 10, 11
+        #dtmp_jdsa = np.diff(dtmp[8:12])
+        #dtmp_jdsa = np.insert(dtmp_jdsa, obj=0, values=(dtmp[11]-dtmp[8]))
+        #restmp_jdsa = np.diff(dists[:,:,8:12], axis=2)
+        #restmp_jdsa = np.insert(restmp_jdsa, obj=0, axis=2, values=(dists[:,:,11]-dists[:,:,8]))
+        ## jdsb idx 12, 13, 14, 15
+        #dtmp_jdsb = np.diff(dtmp[12:16])
+        #dtmp_jdsb = np.insert(dtmp_jdsb, obj=0, values=(dtmp[15]-dtmp[12]))
+        #restmp_jdsb = np.diff(dists[:,:,12:16], axis=2)
+        #restmp_jdsb = np.insert(restmp_jdsb, obj=0, axis=2, values=(dists[:,:,15]-dists[:,:,12]))
+        ## top idx 16, 17, ... 59
+        #dtmp_top = np.diff(dtmp[16:])
+        #dtmp_top = np.insert(dtmp_top, obj=0, values=(dtmp[-1]-dtmp[16]))
+        #restmp_top = np.diff(dists[:,:,16:], axis=2)
+        #restmp_top = np.insert(restmp_top, obj=0, axis=2, values=(dists[:,:,-1]-dists[:,:,16]))
+
+        ## combine all residuals into one matrix
+        #drestmp = np.concatenate((restmp_jdna, restmp_jdnb, restmp_jdsa,
+        #                                   restmp_jdsb, restmp_top), axis=2)
+        ## distance residual (square, sum, sqrt)
+        #drestmp = np.sqrt(np.sum(drestmp**2, axis=2))
+        ## normalize by array size
+        ## sqrt(sum(top)**2/44 + sum(jdna)**2/4)
+        drestmp = np.sqrt(restmp21**2 + restmp34**2 +
+                          restmp46**2 + restmp63**2 + 
+                          restmp78**2 + restmp89**2 + restmp97**2)
+
+
         trestmp = drestmp / 343
 
 
         # add contour lines to plot
     #TODO note originally in for loop -- how to get different lines for different k vals???
-    contour = ax_net.contour(easting, northing, trestmp, cmap='viridis')
+        contour = ax_net.contour(easting, northing, trestmp, 
+                                 levels=np.arange(0.005, 0.015, 0.00125),
+                                 colors=[(1, 0.5, 0.5)])
+                                 #cmap='viridis')
     cbar_net = fig_net.colorbar(contour)
 
 
